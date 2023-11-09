620c389075a8312cb525a50200cf802ddd771603
cavaMetro <cava@seoulmetro.co.kr>
2023-11-10 07:06:30 +0900

    1.5 Late Variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 void main() {
-  // var name1 = 'nico';
-  // String name2 = 'lynn';
+  late String name1; // String
+  late Object name2; // Dynamic
+  name1 = "nico";
+  name2 = "lynn";
 
-  const name1 = 'nico';
-  const String name2 = 'lynn';
+  // name1 = 1;  // A value of type 'int' can't be assigned to a variable of type 'String'.
+  name2 = 2;
+
+  late final String name3;
+  late final String name4;
+
+  // do something, go to api
+  name3 = "nico";
+  name4 = "lynn";
+
+  // name3 = "cava"; // The late final local variable is already assigned.
+  // name4 = 1;  // The late final local variable is already assigned.
 }
