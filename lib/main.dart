f5864e241bef449e43026e1605eeb68cda4739dd
cava <cava@kaist.ac.kr>
2023-11-14 16:20:24 +0900

    2.1~2.3 Lists,String Interpolation,Collection for

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,42 @@
 void main() {
-  String name = "nico";
-  bool alive = true;
+  // String interpolation
+  var name = 'nico';
+  var age = 10;
+  // $변수명
+  // ${변수명을 포함한 계산식}
+  var greeting =
+      "Hello everyone, my name is $name and I'm ${age + 2}, nice to meet you.";
+  print(greeting);
 
-  int age = 12;
-  double money = 69.99;
+  // List
+  var numbers = [1, 2, 3, 4];
+  // List<int> numbers = [1, 2, 3, 4]; // 위와 같음
 
-  // int와 double은 num을 상속
-  // num은 정수, 실수 모두 가능
-  num x = 12;
-  x = 0.1;
+  numbers.add(5);
+  // numbers.add("lalala"); // The argument type 'String' can't be assigned to the parameter type 'int'.
 
-  print(name.length);
-  print(alive.toString());
-  print(age.isEven);
-  print(money.sign);
+  // collection if, collection for 지원
+
+  // collection if
+  // 로그인을 했을 때 나오는 메뉴 등에서 사용
+  var giveMeFive = true;
+  var collectionIfNumbers = [
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5,
+  ];
+
+  print(collectionIfNumbers);
+
+  // collection for
+  var oldFriends = ['nico', 'lynn'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren',
+    for (var friend in oldFriends) "❤️ $friend",
+  ];
+  print(newFriends);
 }
