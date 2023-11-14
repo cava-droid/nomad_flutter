8b4f35831861ac14675ba41f265c073783819f15
cava <cava@kaist.ac.kr>
2023-11-14 16:38:52 +0900

    String Interpolation 틀린 문제

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,7 @@
 void main() {
-  // Sets
-  // 모든 item이 unique
-  // @python Tuple
-  // @javascript Set
+  var name = 'nico';
 
-  var numbersSet = {1, 2, 3, 4};
-  Set<int> numbers2 = {1, 2, 3, 4};
+  print("hi! My name is $name.toUpperCase() !");
 
-  var numbersList = [1, 2, 3, 4];
-
-  numbersSet.add(1);
-  numbersSet.add(1);
-  numbersSet.add(1);
-  numbersSet.add(1);
-
-  numbersList.add(1);
-  numbersList.add(1);
-  numbersList.add(1);
-  numbersList.add(1);
-
-  print(numbersSet);
-  print(numbersList);
+  print("hi! My name is ${name.toUpperCase()} !");
 }
