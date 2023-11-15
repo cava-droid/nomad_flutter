d962387f42509176671264d479e4c62120235c12
cava <cava@kaist.ac.kr>
2023-11-15 14:26:07 +0900

    3.0 Defining a function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+// Defining a function
+
+String sayHello(String name){
+  return "Hello $name nice to meet you.";
+}
+
+// fat arrow syntax
+String sayHello2(String name) => "Hello $name nice to meet you.";
+
 void main() {
-  var name = 'nico';
-
-  print("hi! My name is $name.toUpperCase() !");
-
-  print("hi! My name is ${name.toUpperCase()} !");
+  print(sayHello('nico'));
+  print(sayHello2('lynn'));
 }
