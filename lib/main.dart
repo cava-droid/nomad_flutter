006314af92d07af9a127b70344f3ece768369df6
cava <cava@kaist.ac.kr>
2023-11-16 16:19:58 +0900

    3.3 Optional Positional Parameters

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 // Named Parameters
 
-String sayHello({
-  required String name, 
-  required age, 
-  required country,
-  }){
-  return "Hello $name, you are $age, and you come from $country.";
-}
+String sayHello(String name, int age, [String? country = 'korea']) => "Hello $name, you are $age, and you come from $country.";
+
 
 void main() {
-  print(sayHello(
-    name: 'nico', 
-    age: 20,
-    country: "Cuba",
-    ));
+  print(sayHello('nico', 20,));
 }
