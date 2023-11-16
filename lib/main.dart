9ab8666399a3b124dfa835cca6e27d3e3e9e5d57
cavaYoga520 <asknsolve@hotmail.com>
2023-11-16 16:40:27 +0900

    formatter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 // Named Parameters
 
-String sayHello(String name, int age, [String? country = 'korea']) => "Hello $name, you are $age, and you come from $country.";
-
+String sayHello(String name, int age, [String? country = 'korea']) =>
+    "Hello $name, you are $age, and you come from $country.";
 
 void main() {
-  print(sayHello('nico', 20,));
+  print(sayHello(
+    'nico',
+    20,
+  ));
 }
