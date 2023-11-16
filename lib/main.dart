64d5ae5b05b6377e0acb48f8137ee8789172baaf
cavaYoga520 <asknsolve@hotmail.com>
2023-11-16 16:50:54 +0900

    3.4 QQ Operator

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,37 @@
-// Named Parameters
+// QQ Operator
+// ??
+// ??=
 
-String sayHello(String name, int age, [String? country = 'korea']) =>
-    "Hello $name, you are $age, and you come from $country.";
+// Step 1.
+// String capitalizeName(String? name) {
+//   if (name != null) {
+//     return name.toUpperCase();
+//   }
+//   return 'ANON';
+// }
+
+// Step 2.
+// String capitalizeName(String? name) =>
+//     (name != null) ? name.toUpperCase() : 'ANON';
+
+// Step 3.
+String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANON';
 
 void main() {
-  print(sayHello(
-    'nico',
-    20,
-  ));
+  print(
+    capitalizeName('nico'),
+  );
+  print(
+    capitalizeName(null),
+  );
+
+  String? name2;
+  name2 ??= 'nico';
+  name2 ??= 'another';
+
+  print(name2);
+
+  name2 = null;
+  name2 ??= 'another';
+  print(name2);
 }
