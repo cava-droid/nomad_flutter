3664c5c7ec8e5bc96665e1612f0b00f4da27c80b
cava <cava@kaist.ac.kr>
2023-11-17 13:32:12 +0900

    4.0 Your first dart class

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
-// typedef
+// Your First Class
 
-typedef ListOfInts = List<int>;
-typedef UserInfo = Map<String, String>;
+class Player {
+  // class의 property를 정의할 때는 var 대신 String, int 등을 사용
+  String name = 'nico';
+  int xp = 1500;
 
-// List<int> reverseListOfNumbers(List<int> list) {
-//   var reversed = list.reversed;
-//   return reversed.toList();
-// }
-ListOfInts reverseListOfNumbers(ListOfInts list) {
-  var reversed = list.reversed;
-  return reversed.toList();
-}
+  void sayHello() {
+    print("Hi, my name is $name"); // this는 아래처럼 꼭 필요할 때만 사용하는 것을 권장
 
-// String sayHi(Map<String, String> userInfo) {
-//   return "Hi ${userInfo['name']}";
-// }
-String sayHi(UserInfo userInfo) {
-  return "Hi ${userInfo['name']}!";
+    int xp = 100;
+    print("xp = $xp");
+    print("this.xp = ${this.xp}");
+  }
 }
 
 void main() {
-  print(reverseListOfNumbers([1, 2, 3]));
-  print(sayHi({"name": "nico"}));
+  var player = Player(); // new 불필요
+  print(player.name);
+  player.name = 'lynn';
+  print(player.name);
+
+  player.sayHello();
 }
