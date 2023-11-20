e439232c436128dca135350c93187b54ea42d0b7
cava <cava@kaist.ac.kr>
2023-11-20 16:16:40 +0900

    4.5 Cascade Notation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
-// 4.4 Recap
+// 4.5 Cascade Notation
+// ..의 앞의 .은 가장 가까이 있는 class를 가리킴
 
 class Player {
-  final String name;
+  String name;
   int xp;
   String team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
 
   Player.fromJson(Map<String, dynamic> playerJson)
       : name = playerJson["name"],
@@ -39,15 +46,56 @@ void main() {
 
   // Player player = Player.fromJson(apiData); // 이렇게 하면 안 되고
 
-  for (var element in apiData) {
-    // element는 변수명일 뿐 바꾸어도 됨
-    var player = Player.fromJson(element); // 위의 변수명과 동일하기만 하면 됨
-    player.sayHello();
-  }
+  // for (var element in apiData) {
+  //   // element는 변수명일 뿐 바꾸어도 됨
+  //   var player = Player.fromJson(element); // 위의 변수명과 동일하기만 하면 됨
+  //   player.sayHello();
+  // }
 
   for (var playerJson in apiData) {
     // element는 변수명일 뿐 바꾸어도 됨
     var player = Player.fromJson(playerJson); // 위의 변수명과 동일하기만 하면 됨
     player.sayHello();
   }
+
+  // var nico = Player(
+  //   name: "nico",
+  //   xp: 1200,
+  //   team: "red",
+  // );
+
+  // nico.name = 'las';
+  // nico.xp = 1200000;
+  // nico.team = 'blue';
+
+  var nico = Player(name: "nico", xp: 1200, team: "red")
+    ..name = 'las'
+    ..xp = 1200000
+    ..team = 'blue';
+
+  var abc = nico;
+  print("=============");
+  print("Abc Name = ${abc.name}");
+  print("Abc Team = ${abc.team}");
+  print("Abc XP = ${abc.xp}");
+
+  var potato = nico
+    ..name = 'cava'
+    ..xp = 111
+    ..team = 'red';
+
+  print("=============");
+  print("Abc Name = ${abc.name}");
+  print("Abc Team = ${abc.team}");
+  print("Abc XP = ${abc.xp}");
+
+  print("=============");
+  print("Nico Name = ${nico.name}");
+  print("Nico Team = ${nico.team}");
+  print("Nico XP = ${nico.xp}");
+
+  print("=============");
+  print("Potato Name = ${potato.name}");
+  print("Potato Team = ${potato.team}");
+  print("Potato XP = ${potato.xp}");
 }
