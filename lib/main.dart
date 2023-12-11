1faadc3a6ae0cab762731cb08b85205bb189aa3d
cava <cava@kaist.ac.kr>
2023-12-11 16:08:29 +0900

    4.9 Mixins

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,42 @@
-// 4.8 Inheritance
+// 4.9 Mixins
+// 생성자가 없는 class
+// class에 property들을 추가할 때 사용
 
-class Human {
-  final String name;
-  Human({required this.name});
-  void sayHello() {
-    print("Hi my name is $name");
+class Strong {
+  final double strengthLevel = 1500.99;
+}
+
+class QuickRunner {
+  void runQuick() {
+    print("ruuuuuuuun!");
   }
+}
+
+class Tall {
+  final double height = 1.99;
 }
 
 enum Team { blue, red }
 
-class Player extends Human {
+class Player with Strong, QuickRunner, Tall {
+  // mixin을 사용할 때는 extends가 아니라 with
   final Team team;
 
   Player({
     required this.team,
-    required String name,
-  }) : super(name: name);
+  });
 
-  @override
   void sayHello() {
-    // print("Hi my name is $name"); // 이렇게 복사하여도 되지만
-    super.sayHello();
     print("and I play for $team");
   }
 }
 
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
 void main() {
   var player = Player(
     team: Team.red,
-    name: 'nico',
   );
 }
